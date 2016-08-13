950bcc76fd0acb0d81cd7832ff3d953da62b3061
VEEDOMANIA <hariharan.ganesan4@hotmail.com>
2016-08-13 15:44:05 +0530

    SENDING JENKINS_TRY -1
    Change-Id: I42e253bec7252fb61c5b154b0cffe324ff2595b5

---
+++ JENKINS_TRY/src/code/TEST1.java
@@ -0,0 +1,29 @@
+package code;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TEST1 {
+	
+	
+	@BeforeTest
+	public void setup()
+	{
+		
+		
+	}
+
+	@Test
+	public void test()
+	{
+		
+	}
+	
+	
+	@AfterMethod()
+public void teardown()
+	{
+		
+	}
+}
